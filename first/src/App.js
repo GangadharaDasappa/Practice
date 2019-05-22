285c7a23ec7130702392b8d045b41b2211f5261e
GangadharaDasappa <Gangadhara.iiita.ac.in>
2019-05-22 21:36:05 +0530

    new file:   first/src/AddEntries.js 	modified:   first/src/App.js 	modified:   first/src/Frist.js

--- first/src/App.js
+++ first/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import First from './Frist';
+import AddEntries from './AddEntries';
 
 
 
@@ -7,16 +8,26 @@ class App extends Component {
   state = {
     vogue: [
       {name:"Gnagadhara",age: 30, designation: "Techincal Officer", id : 1},
-      {name:"Amarnath",age: 65, designation: "School Teached", id : 2},
-      {name:"Ahmed Abubuker",age: 30, designation: "Techincal Officer", id : 3},
+      {name:"Amarnath",age: 18, designation: "School Teached", id : 2},
+      {name:"Ahmed Abubuker",age: 15, designation: "Techincal Officer", id : 3},
       {name:"James",age: 20, designation: "Technical Assistant", id : 4}
     ]
   }
+ 
+  addEntries = (add) => {
+    add.id = Math.random();
+    let vogue = [...this.state.vogue, add];
+    this.setState({
+      vogue: vogue
+    })
+  }
+
   render(){
     return (
       <div className="App">
         <h4> Welcome to Root Component </h4>
         <First vogue = {this.state.vogue}/>
+        <AddEntries addEntries={this.addEntries}/>
         
       </div>
     );
--- first/src/Frist.js
+++ first/src/Frist.js
@@ -1,28 +1,77 @@
-import React, { Component } from 'react';
+import React from 'react'; // For UI component
+// import React, { Component } from 'react'; // For Container Component 
 
-
-class First extends Component{
-    render(){
-        const vogue = this.props.vogue;
-        const vogueList = vogue.map(v => {
-            return (
-                    <div className="first" key={v.id}>
-                        <h2>Am in second component</h2>
-                        <div>Name: {v.name} </div>
-                        <div>Age: {v.age} </div>
-                        <div>Designation: {v.designation} </div>
-                    </div>
-            );
-        })
-        return (
-                <div className="vogue-List">
-                    {vogueList}
-                </div>
-        )
+// // Container Component
+// class First extends Component{
+//     render(){
+//         const vogue = this.props.vogue;
+//         const vogueList = vogue.map(v => {
+//             return (
+//                     <div className="first" key={v.id}>
+//                         <h2>Am in second component</h2>
+//                         <div>Name: {v.name} </div>
+//                         <div>Age: {v.age} </div>
+//                         <div>Designation: {v.designation} </div>
+//                     </div>
+//             );
+//         })
+//         return (
+//                 <div className="vogue-List">
+//                     {vogueList}
+//                 </div>
+//         )
         
            
         
+//     }
+// }
+
+
+// Ui/Stateless/Functional Component
+ const First = ({vogue}) => { 
+        // const {vogue} = props; destruction by passing the props directly in to the fuction in the above line
+        // const vogueList = vogue.map(v => {
+        //     if(v.age > 20) {
+        //     return (
+        //             <div className="first" key={v.id}>
+        //                 <h2>Am in second component</h2>
+        //                 <div>Name: {v.name} </div>
+        //                 <div>Age: {v.age} </div>
+        //                 <div>Designation: {v.designation} </div>
+        //             </div>
+        //     );
+        //     } else {
+        //         return null;
+        //     }
+        // })
+        
+        // Conditional Operator
+        // const vogueList = vogue.map(c => {
+        //     return c.age > 20?(
+        //         <div className="first" key={c.id}>
+        //                  <h2>Am in second component</h2>
+        //                 <div>Name: {c.name} </div>
+        //                  <div>Age: {c.age} </div>
+        //                  <div>Designation: {c.designation} </div>
+        //              </div>
+        //     ):null;
+        // })
+        return (
+                <div className="vogue-List">
+                    {
+                    vogue.map(c => {
+                    return c.age > 20?(
+                            <div className="first" key={c.id}>
+                                <h2>Am in second component</h2>
+                                <div>Name: {c.name} </div>
+                                <div>Age: {c.age} </div>
+                                <div>Designation: {c.designation} </div>
+                            </div>
+                        ):null;
+                    })}
+                </div>
+        )                        
     }
-}
+
 
 export default First;
