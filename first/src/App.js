25e1d4d0d33a4a1ee28e3e0c53ded2b693919ec3
GangadharaDasappa <Gangadhara.iiita.ac.in>
2019-05-22 15:05:03 +0530

    modified:   first/src/App.js 	modified:   first/src/Frist.js

--- first/src/App.js
+++ first/src/App.js
@@ -4,11 +4,20 @@ import First from './Frist';
 
 
 class App extends Component {
+  state = {
+    vogue: [
+      {name:"Gnagadhara",age: 30, designation: "Techincal Officer", id : 1},
+      {name:"Amarnath",age: 65, designation: "School Teached", id : 2},
+      {name:"Ahmed Abubuker",age: 30, designation: "Techincal Officer", id : 3},
+      {name:"James",age: 20, designation: "Technical Assistant", id : 4}
+    ]
+  }
   render(){
     return (
       <div className="App">
         <h4> Welcome to Root Component </h4>
-        <First />
+        <First vogue = {this.state.vogue}/>
+        
       </div>
     );
   }
--- first/src/Frist.js
+++ first/src/Frist.js
@@ -3,14 +3,25 @@ import React, { Component } from 'react';
 
 class First extends Component{
     render(){
-        return(
-            <div className="first">
-            <h2>Am in second component</h2>
-            <div>Name: Rahul</div>
-            <div>Age: 36</div>
-            <div>Designation: Prime Minister</div>
-            </div>
+        const vogue = this.props.vogue;
+        const vogueList = vogue.map(v => {
+            return (
+                    <div className="first" key={v.id}>
+                        <h2>Am in second component</h2>
+                        <div>Name: {v.name} </div>
+                        <div>Age: {v.age} </div>
+                        <div>Designation: {v.designation} </div>
+                    </div>
+            );
+        })
+        return (
+                <div className="vogue-List">
+                    {vogueList}
+                </div>
         )
+        
+           
+        
     }
 }
 
