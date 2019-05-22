5dff3051120199f854767555fb25379c1ccec126
Yogesh Gupta <yogesh201996@gmail.com>
2019-05-22 14:18:09 +0530

    Tutorial

---
+++ first/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import First from './Frist';
+
+
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h4> Welcome to Root Component </h4>
+        <First />
+      </div>
+    );
+  }
+  
+}
+
+export default App;
---
+++ first/src/Frist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class First extends Component{
+    render(){
+        return(
+            <div className="first">
+            <h2>Am in second component</h2>
+            <div>Name: Rahul</div>
+            <div>Age: 36</div>
+            <div>Designation: Prime Minister</div>
+            </div>
+        )
+    }
+}
+
+export default First;
